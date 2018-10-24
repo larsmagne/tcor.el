9b6b3914629b177a43745639bee2c3c92cc54836
Lars Ingebrigtsen <larsi@gnus.org>
2018-10-24 19:21:03 +0200

    Fix typos

--- tcor.el
+++ tcor.el
@@ -1,15 +1,15 @@
-;;; tcor.el --- The Comics Journal OCR -*- lexical-binding: t -*-
+;;; tcor.el --- ocr.space OCR -*- lexical-binding: t -*-
 ;; Copyright (C) 2016 Lars Magne Ingebrigtsen
 
 ;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
-;; Keywords: fun, comics
+;; Keywords: tools
 
-;; tcj.el is free software; you can redistribute it and/or modify it
+;; tcor.el is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 
-;; tcj.el is distributed in the hope that it will be useful, but
+;; tcor.el is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
